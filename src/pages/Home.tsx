1a673f4783647c10431e02e03bad7130ae33f26f
neelsen1994 <debayan.sen1994@gmail.com>
2025-10-24 23:57:07 +0000

    Added image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,9 +7,9 @@ export default function Home() {
 
   const carouselImages = [
     '/resource/myart.jpg',
-    'https://images.unsplash.com/photo-1561070791-2526d30994b5?w=500&h=500&fit=crop',
-    'https://images.unsplash.com/photo-1549887534-e3e7ff47855b?w=500&h=500&fit=crop',
-    'https://images.unsplash.com/photo-1549887534-f3bbb00a29f6?w=500&h=500&fit=crop',
+    '/resource/myart2.jpg',
+    '/resource/myart3.jpg',
+    '/resource/myart4.jpg',
   ]
 
   useEffect(() => {
